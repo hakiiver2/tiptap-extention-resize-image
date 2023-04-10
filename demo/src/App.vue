131b0065667ced4d74ee569eaf76775df57643bd
hakiiver2 <uotnnizum12@gmail.com>
2023-04-10 19:25:46 +0900

    add demo

---
+++ demo/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import { ref } from 'vue';
+import { ResizableMedia } from '../../src/components/resizableMedia'
+import { content } from '../../src/components/sampleData'
+
+const editor = useEditor({
+  content: content,
+  extensions: [
+    StarterKit,
+    ResizableMedia,
+  ],
+  editorProps: {
+    attributes: {
+      class: 'focus:outline-none prose mx-auto'
+    }
+  }
+})
+
+</script>
+
+<template>
+    <editor-content :editor="editor" />
+</template>
+
+<style scoped>
+</style>
